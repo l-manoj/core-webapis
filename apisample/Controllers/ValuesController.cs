2ce592c31bcf5ac6c84c29dea344a8e86bd598bc
Manoj Poonati <manojpoonati@gmail.com>
2018-04-10 20:39:36 -0500

    tested on linuxi

--- apisample/Controllers/ValuesController.cs
+++ apisample/Controllers/ValuesController.cs
@@ -13,14 +13,14 @@ namespace apisample.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Docker Deep Dive", "This is a test app created to test Docker", "on windows" };
+            return new string[] { "Docker Deep Dive", "This is a test app created to test Docker", "on linux" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "Docker Deep Dive - This is a test api created to test containerizing an application using Docker on windows ";
+            return "Docker Deep Dive - This is a test api created to test containerizing an application using Docker on linux ";
         }
 
         // POST api/values
